dedb29f915270397e4deb9c4c8a3af6d18a2dbc5
J0es1ick <mrjoeslick@gmail.com>
2024-01-19 23:27:48 +0300

    case 1

---
+++ golang/bilet1/bilet1.go
@@ -0,0 +1,76 @@
+package bilet1
+
+import (
+	"errors"
+)
+
+type Person struct {
+	age int
+	name string
+	status string
+}
+
+func (p Person) GetAge() int {
+	return p.age
+}
+
+func (p Person) GetName() string {
+	return p.name
+}
+
+func (p Person) GetStatus() string {
+	return p.status
+}
+
+func (p* Person) SetAge(age int) error {
+	if age > 0 && age < 150 {
+		p.age = age
+		return nil
+	}
+	var ErrWrongAge = errors.New("age is higher or lower than bound")
+	return ErrWrongAge
+}
+
+func NewPerson(name string, age int) (Person, error) {
+	var p Person = Person {
+		age: age,
+		name: name,
+	}
+	switch {
+		case p.age < 18:
+			p.status = "child"
+	 	case p.age >= 18 && p.age < 60: 
+			p.status = "adult"
+	 	case p.age > 60:
+			p.status = "elderly"
+	}
+	var err = p.SetAge(age)
+	if err != nil {
+	    return p, err
+	}
+	return p, err
+}
+
+func CalculateSum(collection []Person) int {
+	result := 0
+	for _, v := range collection {
+		result += v.GetAge()
+	}
+
+	return result
+}
+
+func CalculateAvgAge(collection []Person) float64 {
+	avg := float64(CalculateSum(collection)) / float64(len(collection))
+	return avg
+}
+
+func TryAdd(collection *[]Person, p Person) bool {
+	for _, x := range *collection {
+		if x.GetName() == p.GetName() && x.GetAge() == p.GetAge() {
+			return false
+		}
+	}
+	*collection = append(*collection, p)
+	return true
+}
--- golang/main.go
+++ golang/main.go
@@ -4,47 +4,28 @@ import (
 	"fmt"
 	"log"
 
-	"isuct.ru/informatics2022/lab3"
-	"isuct.ru/informatics2022/personStruct"
+	"isuct.ru/informatics2022/bilet1"
 )
 
 func main() {
-	fmt.Println("Кремнев Юрий Юрьевич")
+	person1, err := bilet1.NewPerson("Bob", 33)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	a := 1.6
-	x_begin := 1.2
-	x_end := 3.7
-	x_delta := 0.5
+	err = person1.SetAge(32)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	lab3.Solve(a,x_begin,x_end,x_delta)
-
-	person1, err := personStruct.NewPerson("John", "M", 20)
+	person2, err := bilet1.NewPerson("John",64)
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	person2, err := personStruct.NewPerson("Jessica", "Unknown", 19)
-	if err != nil {
-		log.Fatal(err)
-	}
+	collection := make([]bilet1.Person,0)
+	fmt.Println(bilet1.TryAdd(&collection, person1))
+	fmt.Println(bilet1.TryAdd(&collection, person2))
 
-	err = person1.SetAge(25)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = person2.SetGender("F")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%s's age - %d.\n", person1.GetName(), person1.GetAge())
-	person2.KnowGender()
-
-	str, err := person2.GoToWalk(12)
-	if err == nil {
-		fmt.Print(str)
-	} else if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(bilet1.CalculateAvgAge(collection))
 }
